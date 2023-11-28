3670baf66006ba6caff2029d6a9acbedb95d16cc
HarYor785 <haryor785@gmail.com>
2023-11-28 19:24:21 +0100

    changes in utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 import { setPosts } from "../redux/postSlice"
-// import dotenv from "dotenv"
 
-// dotenv.config()
 
-const API_URL = "http://localhost:8000/api-v1"
+const API_URL = "https://mernstack-socialmedia-qxk4.onrender.com"
+
+//"http://localhost:8000/api-v1"
 
 export const API = axios.create({
     baseURL: API_URL,
